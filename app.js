9e869f75449f039f2f9586d5e6c2df3a14cc0dd5
Grey Altr <grey.sifr@gmail.com>
2022-11-01 10:09:05 -0700

    added img reset and score display functions

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 /* Imports */
+
+/* Get DOM Elements */
 const buttonOne = document.getElementById('button1');
 const buttonTwo = document.getElementById('button2');
 const buttonThree = document.getElementById('button3');
@@ -8,9 +10,22 @@ const imgThree = document.getElementById('img3');
 const wins = document.getElementById('wins');
 const losses = document.getElementById('losses');
 const total = document.getElementById('total');
-/* Get DOM Elements */
 
 /* State */
+let wins = 0;
+let total = 0;
+
+function resetImg() {
+    imgOne.src = './assets/cup.png';
+    imgTwo.src = './assets/cup.png';
+    imgThree.src = './assets/cup.png';
+}
+
+function displayScores() {
+    wins.textContent = wins;
+    losses.textContent = total - wins;
+    total.textContent = total;
+}
 
 /* Events */
 
