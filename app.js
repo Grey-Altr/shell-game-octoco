5afaf3d4f76a758af75f14396f86f914eda27160
Grey Altr <grey.sifr@gmail.com>
2022-11-01 09:55:24 -0700

    added img and score-section DOM gets and Moved to get DOM elements section in js

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 const buttonOne = document.getElementById('button1');
 const buttonTwo = document.getElementById('button2');
 const buttonThree = document.getElementById('button3');
+const imgOne = document.getElementById('img1');
+const imgTwo = document.getElementById('img2');
+const imgThree = document.getElementById('img3');
+const wins = document.getElementById('wins');
+const losses = document.getElementById('losses');
+const total = document.getElementById('total');
 /* Get DOM Elements */
 
 /* State */
